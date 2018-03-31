812690212e0ef296df88173bc928c8276462d6ae
lucdandrel <luc.dandrel@hetic.net>
2018-03-31 19:21:35 +0200

    :lipstick: App.vue > add return home

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,9 @@
       <div class="header-logo">
         <router-link tag="a" to="/">Luc <br> Dandrel</router-link>
       </div>
+      <div class="header-return">
+        <router-link tag="a" to="/">return home</router-link>
+      </div>
       <div class="header-about">
         <router-link tag="a" to="/">About</router-link>
       </div>
@@ -154,24 +157,48 @@ export default {
   justify-content: space-between;
   position: fixed;
   top:32px;
-  a{
-      color:$white;
-      font-family: $title-font;
-      position:relative;
+  .header-logo a, .header-about a{
+    font-family: $title-font;
+    position:relative;
+    color:$white;
+    &:before{
+      content:"";
+      width: 1px;
+      height: calc(100% + 1px);
+      background-color:$white;
+      position:absolute;
+      will-change: transform;
+    }
+    &:hover{
       &:before{
-        content:"";
-        width: 1px;
-        height: calc(100% + 1px);
-        background-color:$white;
-        position:absolute;
-        will-change: transform;
-      }
-      &:hover{
-        &:before{
-            animation: scroll-fill 1.2s cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
-        }
+          animation: scroll-fill 1.2s cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
       }
     }
+  }
+
+  .header-return a{
+    color:$white;
+    position:relative;
+    margin-left:-12px;
+    &::after{
+      content:"";
+      width: 20px;
+      height: 1px;
+      position: absolute;
+      background-color:$white;
+      bottom:2px;
+      margin-left:12px;
+      transform-origin: right;
+      will-change: transform;
+    }
+  }
+
+  .header-return:hover{
+      a::after{
+        transform: scaleX(6);
+        transition: all 1s ease;
+      }
+  }
 
   .header-logo{
     margin-left:7%;
