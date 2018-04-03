33eb2671a62ffa3f6d5dbccb986c71181ec69f72
lucdandrel <luc.dandrel@hetic.net>
2018-04-03 22:35:36 +0200

    :lipstick: Header.vue > animation enter

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,31 +1,61 @@
 <template>
     <div class="header">
-        <div class="header-logo">
-            <router-link tag="a" to="/">Luc <br> Dandrel</router-link>
-        </div>
-        <div class="header-return">
-            <router-link tag="a" to="/">return home</router-link>
-        </div>
-        <div class="header-about">
-            <router-link tag="a" to="/">About</router-link>
+        <div class="header-wrapper" ref="header">
+          <div class="header-logo">
+              <router-link tag="a" to="/">Luc <br> Dandrel</router-link>
+          </div>
+          <div class="header-return" v-if="$route.name == 'project'">
+              <router-link tag="a" to="/">return home</router-link>
+          </div>
+          <div class="header-about">
+              <router-link tag="a" to="/">About</router-link>
+          </div>
         </div>
     </div>
 </template>
 
 <script>
+import { TweenMax } from 'gsap';
+import { ease } from '@/services/utils';
+
 export default {
-  name: 'Header'
+  name: 'Header',
+  methods: {
+    enterAnimation() {
+      TweenMax.to(this.$refs.header, 1, {
+        y: '0%',
+        ease: ease,
+        delay: 1
+      });
+    }
+  },
+  watch: {
+    websiteReady(boolean) {
+      if (boolean === true) {
+        this.enterAnimation();
+      }
+    }
+  },
+  computed: {
+    websiteReady() {
+      return this.$store.state.websiteReady;
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .header{
   width: 100vw;
-  display: flex;
-  align-items: flex-end;
-  justify-content: space-between;
   position: fixed;
   top:32px;
+  overflow: hidden;
+  .header-wrapper{
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    transform: translateY(100%);
+  }
   .header-logo a, .header-about a{
     font-family: $title-font;
     position:relative;
